559441a7c642cd54ffa5a7ea77cf248cb127e4d5
Mitch Aplin <mitchaplin@gmail.com>
2022-09-02 16:30:19 -0500

    Initial project setup

---
+++ src/2015/input.js
@@ -0,0 +1,7 @@
+import fs from 'fs';
+
+export const input = fs
+	.readFileSync('./input.txt', 'utf8')
+	.toString()
+	.trim()
+	.split('');
---
+++ src/2015/part-one.js
@@ -0,0 +1,5 @@
+import { frequencies } from '../utils.js';
+import { input } from './input.js';
+
+
+console.log(frequencies(input));
