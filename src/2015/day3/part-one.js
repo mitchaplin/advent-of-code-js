f9c90dffff4b628f3a47672997acedf2616473b1
Mitch Aplin <mitchaplin@gmail.com>
2022-09-04 14:36:48 -0500

    Bring over some useful utility functions 2015 Day 3 - Flesh out some much needed utility functions

--- src/2015/day3/part-one.js
+++ src/2015/day3/part-one.js
@@ -1,5 +1,30 @@
+import { isArrUniqMultiplePropsFilter } from '../../utils.js';
 import { input } from './input.js';
 
+const translateInst = (inst, { x, y }) => {
+	switch (inst) {
+		case '^':
+			return { x, y: ++y };
+		case 'v':
+			return { x, y: --y };
+		case '<':
+			return { x: --x, y };
+		case '>':
+			return { x: ++x, y };
+	}
+};
+
+const moveSanta = () => {
+	let currentLocation = { x: 0, y: 0 };
+	let visitedLocations = [currentLocation];
+	for (const inst of input) {
+		currentLocation = translateInst(inst, currentLocation);
+		visitedLocations.push(currentLocation);
+	}
+
+	return isArrUniqMultiplePropsFilter(visitedLocations, ['x', 'y']);
+};
+
 console.time();
-console.log(input);
+console.log(moveSanta().length);
 console.timeEnd();
