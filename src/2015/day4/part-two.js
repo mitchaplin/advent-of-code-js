d968e0c19271873e34f33befab25d0641b4907ad
Mitch Aplin <mitchaplin@gmail.com>
2022-09-05 14:35:06 -0500

    2015 Day 4 complete

--- src/2015/day4/part-two.js
+++ src/2015/day4/part-two.js
@@ -1,5 +1,15 @@
+import MD5 from 'crypto-js/md5.js';
 import { input } from './input.js';
 
+const getHash = () => {
+	let counter = 0;
+	let currentHash = `${input}${counter}`;
+	while (!currentHash.startsWith('000000')) {
+		++counter;
+		currentHash = MD5(`${input}${counter}`).toString();
+	}
+	return counter;
+};
 console.time();
-console.log(input);
+console.log(getHash());
 console.timeEnd();
