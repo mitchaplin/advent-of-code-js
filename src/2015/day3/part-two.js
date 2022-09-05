8e35cd57670b5918d742c193337b9b0f8b0708ee
Mitch Aplin <mitchaplin@gmail.com>
2022-09-04 20:38:58 -0500

    2015 Day3 part 2

--- src/2015/day3/part-two.js
+++ src/2015/day3/part-two.js
@@ -1,5 +1,35 @@
+import { isArrUniqMultiplePropsFilter } from '../../utils.js';
 import { input } from './input.js';
 
+const aOdd = input.filter((_, i) => i % 2 === 1);
+const aEven = input.filter((_, i) => i % 2 === 0);
+
+const translateInst = (inst, { x, y }) => {
+	switch (inst) {
+		case '^':
+			return { x, y: ++y };
+		case 'v':
+			return { x, y: --y };
+		case '<':
+			return { x: --x, y };
+		case '>':
+			return { x: ++x, y };
+	}
+};
+
+const moveSanta = (instructions) => {
+	let currentLocation = { x: 0, y: 0 };
+	let visitedLocations = [currentLocation];
+	for (const inst of instructions) {
+		currentLocation = translateInst(inst, currentLocation);
+		visitedLocations.push(currentLocation);
+	}
+
+	return visitedLocations;
+};
+
 console.time();
-console.log(input);
+console.log(
+	isArrUniqMultiplePropsFilter([...moveSanta(aOdd), ...moveSanta(aEven)], ['x', 'y']).length
+);
 console.timeEnd();
