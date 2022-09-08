c5f81d03fbe695f737bebbd7b5b2bb07e723ef2d
Mitch Aplin <mitchaplin@gmail.com>
2022-09-07 22:17:32 -0500

    Begin work on Day5 part 2

--- src/2015/day5/part-two.js
+++ src/2015/day5/part-two.js
@@ -1,19 +1,42 @@
-import { partitionArray } from '../../utils.js';
+import { frequencies, partitionArray, partitionArrayByThree } from '../../utils.js';
 import { input } from './input.js';
 
 const partitionedChecks = (inp) => {
 	const partitioned = partitionArray(inp.split(''));
+	const partitionedByThree = partitionArrayByThree(inp.split(''));
+	const setCheck = Object.values(frequencies(partitioned)).filter((x) => x > 1);
+	console.log(
+		partitionedByThree.filter((item) => {
+			return item[0] === item[1] && item[1] === item[2];
+		}),
+		setCheck.length,
+
+		setCheck.length > 0 &&
+			//includes two of the same letters in positions 0/2 in sequence of 0 1 2
+			partitionedByThree.filter((item) => {
+				return item[0] === item[2];
+			}).length > 0 &&
+			//does not include all three of the same letters in positions 0/1/2 in sequence of 0 1 2
+			!partitionedByThree.filter((item) => {
+				return setCheck.length === 1 && item[0] === item[1] && item[1] === item[2];
+			}).length > 0
+	);
 	return (
-		//includes b2b letters
-		partitioned.filter((letterPair) => partitioned.includes()).length >= 1 &&
-		// does NOT include naughties
-		partitioned.length - partitioned.filter((item) => !naughtyString.includes(item)).length ===
-			0
+		//includes two pairs s letters
+		setCheck.length > 0 &&
+		//includes two of the same letters in positions 0/2 in sequence of 0 1 2
+		partitionedByThree.filter((item) => {
+			return item[0] === item[2];
+		}).length > 0 &&
+		//does not include all three of the same letters in positions 0/1/2 in sequence of 0 1 2
+		!partitionedByThree.filter((item) => {
+			return setCheck <= 2 && item[0] === item[1] && item[1] === item[2];
+		}).length > 0
 	);
 };
 
 const filterInputs = () => {
-	const remainingStrings = input.filter((inp) => vowelCheck(inp) && partitionedChecks(inp));
+	const remainingStrings = input.filter((inp) => partitionedChecks(inp));
 	return remainingStrings.length;
 };
 
