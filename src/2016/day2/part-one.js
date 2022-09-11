b577e8b642a296a383706181090c66c28011c071
Mitch Aplin <mitchaplin@gmail.com>
2022-09-10 21:51:06 -1000

    2016 Day 2

--- src/2016/day2/part-one.js
+++ src/2016/day2/part-one.js
@@ -1,5 +1,41 @@
 import { input } from './input.js';
 
+const board = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9],
+];
+
+const moveCoord = (inst, { x, y }) => {
+	switch (inst) {
+		case 'D':
+			return { x: x, y: y >= 2 ? 2 : ++y };
+		case 'U':
+			return { x: x, y: y <= 0 ? 0 : --y };
+		case 'L':
+			return { x: x <= 0 ? 0 : --x, y: y };
+		case 'R':
+			return { x: x >= 2 ? 2 : ++x, y: y };
+	}
+};
+
+const getBoardNumber = ({ x, y }) => {
+	return board[y][x];
+};
+
+const getCode = () => {
+	let code = '';
+	let newLoc = { x: 1, y: 1 };
+	let codeArr = [];
+	for (const inst of input) {
+		inst.split('').map((letter) => {
+			newLoc = moveCoord(letter, newLoc);
+		});
+		code = getBoardNumber(newLoc);
+		codeArr.push(code);
+	}
+	return codeArr;
+};
 console.time();
-console.log(input);
+console.log(getCode().join(''));
 console.timeEnd();
--- src/2016/day2/part-two.js
+++ src/2016/day2/part-two.js
@@ -1,5 +1,110 @@
 import { input } from './input.js';
 
+const board = [[1], [2, 3, 4], [5, 6, 7, 8, 9], ['A', 'B', 'C'], ['D']];
+
+//     1
+//   2 3 4
+// 5 6 7 8 9
+//   A B C
+//     D
+
+const checkXBounds = (inst, x, y) => {
+	switch (inst) {
+		case 'L':
+			if (y === 0) {
+				return { x: x, y: y };
+			} else if (y === 1) {
+				return { x: x === 1 ? x : --x, y: y };
+			} else if (y === 2) {
+				return { x: x === 0 ? x : --x, y: y };
+			} else if (y === 3) {
+				return { x: x === 1 ? x : --x, y: y };
+			} else if (y === 4) {
+				return { x: x, y: y };
+			}
+
+		case 'R':
+			if (y === 0) {
+				return { x: x, y: y };
+			} else if (y === 1) {
+				return { x: x === 3 ? x : ++x, y: y };
+			} else if (y === 2) {
+				return { x: x === 4 ? x : ++x, y: y };
+			} else if (y === 3) {
+				return { x: x === 3 ? x : ++x, y: y };
+			} else if (y === 4) {
+				return { x: x, y: y };
+			}
+	}
+};
+//     1
+//   2 3 4
+// 5 6 7 8 9
+//   A B C
+//     D
+const checkYBounds = (inst, x, y) => {
+	console.log(inst, x, y);
+	switch (inst) {
+		case 'U':
+			if (x === 0) {
+				return { x: x, y: y };
+			} else if (x === 1) {
+				return { x: x, y: y === 1 ? y : --y };
+			} else if (x === 2) {
+				return { x: x, y: y === 0 ? y : --y };
+			} else if (x === 3) {
+				return { x: x, y: y === 1 ? y : --y };
+			} else if (x === 4) {
+				return { x: x, y: y };
+			}
+		case 'D':
+			if (x === 0) {
+				return { x: x, y: y };
+			} else if (x === 1) {
+				return { x: x, y: y === 3 ? y : ++y };
+			} else if (x === 2) {
+				return { x: x, y: y === 4 ? y : ++y };
+			} else if (x === 3) {
+				return { x: x, y: y === 3 ? y : ++y };
+			} else if (x === 4) {
+				return { x: x, y: y };
+			}
+	}
+};
+
+const moveCoord = (inst, { x, y }) => {
+	switch (inst) {
+		case 'D':
+			return checkYBounds('D', x, y);
+		case 'U':
+			return checkYBounds('U', x, y);
+		case 'L':
+			return checkXBounds('L', x, y);
+		case 'R':
+			return checkXBounds('R', x, y);
+	}
+};
+
+const getBoardNumber = ({ x, y }) => {
+	console.log(y, x);
+	return board[y][x];
+};
+
+const getCode = () => {
+	let code = '';
+	let newLoc = { x: 0, y: 3 };
+	let codeArr = [];
+	for (const inst of input) {
+		inst.split('').map((letter) => {
+			newLoc = moveCoord(letter, newLoc);
+		});
+		console.log(newLoc);
+		code = getBoardNumber(newLoc);
+		codeArr.push(code);
+	}
+	return codeArr;
+};
+
 console.time();
-console.log(input);
+console.log(getCode());
 console.timeEnd();
