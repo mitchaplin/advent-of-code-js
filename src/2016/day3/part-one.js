5aee3c3db3d06f744fb6b77068eefb5d6d6f206c
Mitch Aplin <mitchaplin@gmail.com>
2022-09-15 17:45:58 -1000

    2016 Day 3-1

--- src/2016/day3/part-one.js
+++ src/2016/day3/part-one.js
@@ -1,5 +1,31 @@
 import { input } from './input.js';
 
+const sortInputs = (x) => {
+	return x[0]
+		.split(/\s+/)
+		.map((y) => parseInt(y))
+		.sort((a, b) => a - b);
+};
+
+const isValidTriangle = (arr) => {
+	let a, b, c;
+	[a, b, c] = arr;
+
+	return a + b > c;
+};
+
+const compute = () => {
+	let adjustedArr = [];
+	let adjustedInput;
+	for (const inp of input) {
+		adjustedInput = sortInputs(inp);
+		if (isValidTriangle(adjustedInput)) {
+			adjustedArr.push(adjustedInput);
+		}
+	}
+	return adjustedArr.length;
+};
+
 console.time();
-console.log(input);
+console.log(compute());
 console.timeEnd();
