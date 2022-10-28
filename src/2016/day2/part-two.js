c640df491dba4fcf1fe04cc6027d3a76eb1e581a
Mitch Aplin <mitchaplin@gmail.com>
2022-10-28 13:49:50 -0500

    2015-6 and 2016-2 cleanup

--- src/2016/day2/part-two.js
+++ src/2016/day2/part-two.js
@@ -1,78 +1,56 @@
 import { input } from './input.js';
 
-const board = [[1], [2, 3, 4], [5, 6, 7, 8, 9], ['A', 'B', 'C'], ['D']];
-
-//     1
-//   2 3 4
-// 5 6 7 8 9
-//   A B C
-//     D
+const board = [
+	[null, null, null, null, null, null, null],
+	[null, null, null, 1, null, null, null],
+	[null, null, 2, 3, 4, null, null],
+	[null, 5, 6, 7, 8, 9, null],
+	[null, null, 'A', 'B', 'C', null, null],
+	[null, null, null, 'D', null, null, null],
+	[null, null, null, null, null, null, null],
+];
 
 const checkXBounds = (inst, x, y) => {
 	switch (inst) {
 		case 'L':
-			if (y === 0) {
+			let leftX = board[y][x - 1];
+			if (leftX === null) {
 				return { x: x, y: y };
-			} else if (y === 1) {
-				return { x: x === 1 ? x : --x, y: y };
-			} else if (y === 2) {
-				return { x: x === 0 ? x : --x, y: y };
-			} else if (y === 3) {
-				return { x: x === 1 ? x : --x, y: y };
-			} else if (y === 4) {
-				return { x: x, y: y };
+			} else {
+				return { x: x - 1, y: y };
 			}
 
 		case 'R':
-			if (y === 0) {
+			let rightX = board[y][x + 1];
+			if (rightX === null) {
 				return { x: x, y: y };
-			} else if (y === 1) {
-				return { x: x === 3 ? x : ++x, y: y };
-			} else if (y === 2) {
-				return { x: x === 4 ? x : ++x, y: y };
-			} else if (y === 3) {
-				return { x: x === 3 ? x : ++x, y: y };
-			} else if (y === 4) {
-				return { x: x, y: y };
+			} else {
+				return { x: x + 1, y: y };
 			}
 	}
 };
-//     1
-//   2 3 4
-// 5 6 7 8 9
-//   A B C
-//     D
+
 const checkYBounds = (inst, x, y) => {
-	console.log(inst, x, y);
 	switch (inst) {
 		case 'U':
-			if (x === 0) {
+			let upY = board[y - 1][x];
+			if (upY === null) {
 				return { x: x, y: y };
-			} else if (x === 1) {
-				return { x: x, y: y === 1 ? y : --y };
-			} else if (x === 2) {
-				return { x: x, y: y === 0 ? y : --y };
-			} else if (x === 3) {
-				return { x: x, y: y === 1 ? y : --y };
-			} else if (x === 4) {
-				return { x: x, y: y };
+			} else {
+				return { x: x, y: y - 1 };
 			}
 		case 'D':
-			if (x === 0) {
+			let downY = board[y + 1][x];
+			if (downY === null) {
 				return { x: x, y: y };
-			} else if (x === 1) {
-				return { x: x, y: y === 3 ? y : ++y };
-			} else if (x === 2) {
-				return { x: x, y: y === 4 ? y : ++y };
-			} else if (x === 3) {
-				return { x: x, y: y === 3 ? y : ++y };
-			} else if (x === 4) {
-				return { x: x, y: y };
+			} else {
+				return { x: x, y: y + 1 };
 			}
 	}
 };
 
 const moveCoord = (inst, { x, y }) => {
+	let newCoord;
 	switch (inst) {
 		case 'D':
 			return checkYBounds('D', x, y);
@@ -83,26 +61,24 @@ const moveCoord = (inst, { x, y }) => {
 		case 'R':
 			return checkXBounds('R', x, y);
 	}
+	return newCoord;
 };
 
 const getBoardNumber = ({ x, y }) => {
-	console.log(y, x);
 	return board[y][x];
 };
 
 const getCode = () => {
 	let code = '';
-	let newLoc = { x: 0, y: 3 };
-	let codeArr = [];
+	let newLoc = { x: 1, y: 3 };
+
 	for (const inst of input) {
-		inst.split('').map((letter) => {
+		inst.split('').map((letter, idx) => {
 			newLoc = moveCoord(letter, newLoc);
 		});
-		console.log(newLoc);
-		code = getBoardNumber(newLoc);
-		codeArr.push(code);
+		code += getBoardNumber(newLoc);
 	}
-	return codeArr;
+	return code;
 };
 
 console.time();
